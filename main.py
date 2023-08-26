2c86e7ef7ec8dd12863227dd6642892f15c2134d
namseokyoo <namseok.yoo@gmail.com>
2023-08-26 21:01:41 +0900

    deploy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from dotenv import load_dotenv
-load_dotenv()
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3']=sys.modules.pop('pysqlite3')
+
+
+# from dotenv import load_dotenv
+# load_dotenv()
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
